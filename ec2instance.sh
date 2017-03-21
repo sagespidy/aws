ddb4d41f4bdb2d1624ba008bf7bf673f1e30193a
[sagespidy] <[shailender.birmaan2@gmail.com]>
2017-03-21 10:04:25 +0530

    successfully working

--- ec2instance.sh
+++ ec2instance.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 echo
-echo 
+echo
 echo 				"Welcome! This script will create a security group"
 echo
 echo "Please enter the name of group to created in aws: "
-# Ask the user for aws security group name to be 
+# Ask the user for aws security group name to be
 read g_name
 
 # Command to create a aws  security group and output the group details into a file
 
 
-aws ec2 create-security-group --group-name $g_name --description "security group for $g_name development environment in EC2" > $g_name.secgrp 
+aws ec2 create-security-group --group-name $g_name --description "security group for $g_name development environment in EC2" > $g_name.secgrp
 
 
 echo
@@ -49,9 +49,8 @@ echo "Please enter the name of key-pair to be created:"
 read kp
 aws ec2 create-key-pair --key-name $kp --query 'KeyMaterial' --output text > $kp.pem
 echo -e "\n\n\n"
-echo "Key-pair Created SuccessFully.\n You can find the pem file \"$kp.pem\" in /Users/Spidy/pem/ "
+echo "Key-pair Created SuccessFully.\n  "
 chmod 400 $kp.pem
-mv $kp.pem ~/cpem
 #
 #
 # finally Launch instance with ubuntu
@@ -63,6 +62,6 @@ echo "Please enter the type of instance (Example format : t2.small)" :
 read i_type
 
 # Launch ec2 with exiting sec grp and key pair
-aws ec2 run-instances --image-id ami-e13739f6 --security-group-ids $g_id --count 1 --instance-type $i_type --key-name $kp --query 'Instances[0].InstanceId'
+aws ec2 run-instances --image-id ami-e13739f6 --security-group-ids $g_id --count 1 --instance-type $i_type --key-name $kp --query 'Instances[0].InstanceId' --block-device-mappings file://~/aws/size.json
 
 echo "instance created."
