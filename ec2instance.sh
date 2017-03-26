8698ea7b289b82c8727c63e813ca23d1171b64d3
[sagespidy] <[shailender.birmaan2@gmail.com]>
2017-03-27 03:16:21 +0530

    able to launch instance

--- ec2instance.sh
+++ ec2instance.sh
@@ -2,26 +2,24 @@
 
 echo
 echo
-echo 				"Welcome! This script will create a security group"
+echo 				"Welcome! This scripat will create a security group"
 echo
 echo "Please enter the name of group to created in aws: "
 # Ask the user for aws security group name to be
 read g_name
 
 # Command to create a aws  security group and output the group details into a file
-
-
-aws ec2 create-security-group --group-name $g_name --description "security group for $g_name development environment in EC2" > $g_name.secgrp
-
-
+#ws ec2 create-security-group --group-name $g_name --description "security group for $g_name development environment in EC2" > $g_name.secgrp
+# Modified command which automatically store out into the variable.Optimized the code to stop make un-necessary file
+g_id=`aws ec2 create-security-group --group-name $g_name --description "security group for $g_name development environment in EC2" | grep sg | cut -d '"' -f4`
 echo
 echo
-echo -e "The security group has been created. \n "
+echo  "The security group has been created.  "
 echo
 echo
 ##
 ## store group id in a var
-g_id=`cat $g_name.secgrp | grep sg | cut -d '"' -f4`
+#g_id=`cat $g_name.secgrp | grep sg | cut -d '"' -f4`
 echo "group id is $g_id"
 
 ##
@@ -45,12 +43,23 @@ echo -e "By Default, the following ports are enabled: \n 22		SSH\n 80		HTTP\n 44
 echo " Welcome! This script will create a key pair "
 echo -e "\n\n\n"
 
-echo "Please enter the name of key-pair to be created:"
-read kp
-aws ec2 create-key-pair --key-name $kp --query 'KeyMaterial' --output text > $kp.pem
-echo -e "\n\n\n"
-echo "Key-pair Created SuccessFully.\n  "
-chmod 400 $kp.pem
+# shit goes here
+read -p "Do you want to create a new pem file " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  echo "Please enter the name of key-pair to be created:"
+  read kp
+  aws ec2 create-key-pair --key-name $kp --query 'KeyMaterial' --output text > $kp.pem
+  echo -e "\n\n\n"
+  echo "Key-pair Created SuccessFully.\n  "
+  chmod 400 $kp.pem
+
+else
+	echo "enter name of pem file | Just name - don't include .pem extension"
+	read kp
+	echo $kp.pem
+fi
 #
 #
 # finally Launch instance with ubuntu
@@ -62,7 +71,7 @@ echo "Please enter the type of instance (Example format : t2.small)" :
 read i_type
 
 # Launch ec2 with exiting sec grp and key pair
-aws ec2 run-instances --image-id ami-e13739f6 --security-group-ids $g_id --count 1 --instance-type $i_type --key-name $kp --query 'Instances[0].InstanceId' --block-device-mappings  '{"DeviceName": "/dev/sda1","Ebs": {"VolumeSize": 30}}'
+aws ec2 run-instances --image-id ami-a58d0dc5 --security-group-ids $g_id --count 1 --instance-type $i_type --key-name $kp --query 'Instances[0].InstanceId' --block-device-mappings  '{"DeviceName": "/dev/sda1","Ebs": {"VolumeSize": 30}}' --user-data file:///Users/spidy/shell-scripts/non-interactive-apache2.sh
 
 
 echo "instance created."
