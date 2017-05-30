23f03d68ca1688e771210f3c6bbea19fa4621805
Shailender Choudhary <shailender.birmaan2@gmail.com>
2017-05-30 17:55:34 +0530

    idk, old modified

--- ec2instance.sh
+++ ec2instance.sh
@@ -9,22 +9,15 @@ echo "Please enter the name of security group: "
 # Ask the user for aws security group name to be
 read g_name
 
-# Command to create a aws  security group and output the group details into a file
-#ws ec2 create-security-group --group-name $g_name --description "security group for $g_name development environment in EC2" > $g_name.secgrp
-# Modified command which automatically store out into the variable.Optimized the code to stop make un-necessary file
 g_id=`aws ec2 create-security-group --group-name $g_name --description "security group for $g_name development environment in EC2" | grep sg | cut -d '"' -f4`
 echo
 echo
 echo  "The security group has been created.  "
 echo
 echo
-##
-## store group id in a var
-#g_id=`cat $g_name.secgrp | grep sg | cut -d '"' -f4`
+
 echo "group id is $g_id"
 
-##
-##
 # Command to add open  ports in Above created security group
 
 #enable ssh
@@ -39,9 +32,9 @@ echo -e "By Default, the following ports are enabled: \n 22		SSH\n 80		HTTP\n 44
 ###
 ### Create pem file
 ####
+clear
 
-
-echo " Welcome! This script will create a key pair "
+echo " The  key pair "
 echo -e "\n\n\n"
 
 # shit goes here
@@ -55,17 +48,14 @@ then
   echo -e "\n\n\n"
   echo "Key-pair Created SuccessFully.\n  "
   chmod 400 $kp.pem
-
+  mv $kp.pem ~/
 else
 	echo "enter name of pem file | Just name - don't include .pem extension"
 	read kp
 	echo $kp.pem
 fi
-#
-#
-# finally Launch instance with ubuntu
 
-#
+# finally Launch instance with ubuntu
 
 echo -e " \n\n\n\ "
 echo "Please enter the type of instance (Example format : t2.small)" :
